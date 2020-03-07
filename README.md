7cd85519add9a076f6ee1171679663098a1d3eb4
ksz0x59 <szczerba.krzysztof89@gmail.com>
2020-03-07 22:23:50 +0100

    adding solutions to Exercise1

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Exercise One
 
 1. In The class called PetRepository you can find an example map. Implement A Service Class with the methods that will find and display :
 
-    - Pets that are owned by people over 18 years old
-    - Not Dangerous pets wages less than six 
-    - Owners who are over 20 years old
-    - Owners who have only one pet
-    - An owner who has the most pets 
-    - Sum of wages of all pets which are Dangerous
-    - Dangerous and heaviest pet and also its owner  
+    - pets that are owned by people over 18 years old
+    - not dangerous pets lighter than six kilogrames  
+    - owners who are over 20 years old
+    - owners who have only one pet
+    - an owner who has the most pets 
+    - sum of the weight of all dangerous pets
+    - dangerous and heaviest pet and its owner  
--- src/main/java/com/ksz/one/PetService.java
+++ src/main/java/com/ksz/one/PetService.java
@@ -7,42 +7,139 @@ public class PetService {
 
     private PetRepository repository;
 
-    PetService () {}
+    PetService() {
+    }
 
-    PetService (PetRepository petRepository)
-    {
+    PetService(PetRepository petRepository) {
         this.repository = petRepository;
     }
 
-    public  Set<Pet> petsOfAgePeople() {
-        return null;
+    public Set<Pet> petsOfAgePeople() {
+        Map<Pet, Person> petsMap = repository.getAllPets();
+
+        Set<Pet> petSet = petsMap.entrySet().stream()
+                .filter(e -> e.getValue().getAge() > 17)
+                .map(e -> e.getKey())
+                .collect(Collectors.toSet());
+        return petSet;
     }
 
-    public  Set<Pet> dangerousPetsWagesLessThanSix() {
-        return null;
+    public Set<Pet> dangerousPetsWagesLessThanSix() {
+        Map<Pet, Person> petsMap = repository.getAllPets();
+
+        Set<Pet> petSet = petsMap.keySet().stream()
+                .filter(pet -> pet.isDangerous())
+                .filter(pet -> pet.getWeight() < 6)
+                .collect(Collectors.toSet());
+        return petSet;
     }
 
-    public  Set<Person> peopleOlderThanTwenty() {
-        return null;
+    public Set<Person> peopleOlderThanTwenty() {
+        Map<Pet, Person> petsMap = repository.getAllPets();
+
+        Set<Person> personSet = petsMap.values().stream()
+                .filter(person -> person.getAge() > 20)
+                .collect(Collectors.toSet());
+
+        return personSet;
     }
 
     public Set<Person> peopleWithOnePet() {
-        return null;
+
+        Map<Pet, Person> petsMap = repository.getAllPets();
+
+        Map<Person, List<Pet>> personPetMap = new HashMap<>();
+
+        for (Map.Entry<Pet, Person> entry : petsMap.entrySet()) {
+            Person oldVal = entry.getValue();
+            Pet oldKey = entry.getKey();
+
+            List<Pet> newVal = null;
+
+            if (personPetMap.containsKey(oldVal)) {
+                newVal = personPetMap.get(oldVal);
+                newVal.add(oldKey);
+            } else {
+                newVal = new ArrayList<>();
+                newVal.add(oldKey);
+            }
+
+            personPetMap.put(oldVal, newVal);
+        }
+
+        /*personPetMap.entrySet().forEach( e -> {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+            System.out.println("--------------");
+        });*/
+
+        return personPetMap.entrySet().stream()
+                .filter(e -> e.getValue().size() == 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
     }
 
     public Set<Person> peopleWithOnePetV2() {
-        return null;
+        Map<Pet, Person> petsMap = repository.getAllPets();
+
+        Map<Person, List<Pet>> personPetMap = petsMap.entrySet().stream()
+                .collect(Collectors
+                        .groupingBy((Map.Entry::getValue), Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
+
+        /*personPetMap.entrySet().forEach( e -> {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+            System.out.println("--------------");
+        });*/
+
+        return personPetMap.entrySet().stream()
+                .filter(e -> e.getValue().size() == 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
     }
 
     public Person personWithMaxPets() {
-        return null;
+        Map<Pet, Person> petsMap = repository.getAllPets();
+
+        Map<Person, List<Pet>> personPetMap = petsMap.entrySet().stream()
+                .collect(Collectors
+                        .groupingBy((Map.Entry::getValue), Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
+
+        /*personPetMap.entrySet().forEach( e -> {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+            System.out.println("--------------");
+        });*/
+
+
+        TreeSet<Integer> petCountSet = new TreeSet<>();
+
+        for (Map.Entry<Person, List<Pet>> entry : personPetMap.entrySet()) {
+            petCountSet.add(entry.getValue().size());
+        }
+
+        return personPetMap.entrySet().stream()
+                .filter(e -> e.getValue().size() == petCountSet.last())
+                .map(Map.Entry::getKey).findFirst().get();
     }
 
     public Integer sumDangerousPetsWeight() {
-        return null;
+        Map<Pet, Person> petsMap = repository.getAllPets();
+
+        return petsMap.keySet().stream()
+                .filter(Pet::isDangerous)
+                .mapToInt(Pet::getWeight).sum();
     }
 
     public Map.Entry<Pet, Person> getMostDangerousPetAndOwner() {
-        return null;
+        Map<Pet, Person> petsMap = repository.getAllPets();
+
+        int MaxWeightOfDangerousPet = petsMap.keySet().stream()
+                .filter(Pet::isDangerous)
+                .mapToInt(Pet::getWeight).max().orElse(-1);
+
+        return petsMap.entrySet().stream()
+                .filter(e -> e.getKey().getWeight() == MaxWeightOfDangerousPet)
+                .findFirst().get();
     }
 }
